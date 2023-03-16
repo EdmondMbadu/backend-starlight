f470257daede687e765d8acdaec075deb018f469
Edmond Mbadu <mbadungoma@gmail.com>
2023-03-15 22:47:37 -0400

    adding to UI

--- app.py
+++ app.py
@@ -321,4 +321,4 @@ def delete_post(post_id):
 # running the server
 if __name__ == '__main__':
     # to allow for debugging and auto-reload
-    app.run(host='localhost', port=5000, debug=True)
+    app.run()
